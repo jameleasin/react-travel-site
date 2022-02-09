a5404f38af13a5a49a5e0865fb43913b0412e5ba
Jamie <Sinclair>
2022-02-09 21:31:10 +0800

    application files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import NavBar from "./components/NavBar"
+import { BrowserRouter as Router,  Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <NavBar/>
+        <Routes>
+           <Route path="/" exact />
+         </Routes>
+    </Router>
+      
+      </>
+
+  );
+}
+
+export default App;
