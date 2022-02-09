772ef43760cba3bf11d78acce9407597ab441aab
Jamie <Sinclair>
2022-02-09 22:58:43 +0800

    pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-
-import './App.css';
-import NavBar from "./components/NavBar"
-import Home from './components/pages/home';
-import { BrowserRouter as Router,  Route, Routes } from 'react-router-dom';
 import React from 'react';
+import Navbar from './components/NavBar';
+import './App.css';
+import Home from './components/pages/home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Services from './components/pages/services';
+import Products from './components/pages/products';
+import SignUp from './components/pages/signup';
+import HeroSection from "./components/HeroSection"
+
 function App() {
   return (
     <>
-    <Router>
-      <NavBar/>
+      <Router>
+        <Navbar />
         <Routes>
           <Route path='/' exact component={Home} />
-           
-         </Routes>
-    </Router>
-      </>
-
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/signup' component={SignUp} />
+        </Routes>
+      </Router>
+      <HeroSection />
+    </>
   );
 }
 
